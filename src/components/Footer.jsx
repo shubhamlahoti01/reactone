62fcbbc75887ea9b5505d76fb638d16a2c1ea454
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-01-08 23:37:03 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href='https://www.linkedin.com/in/shubham-lahoti/'
+            target={'blank'}
+          >
+            LinkedIn
+          </a>
+          <a href='https://instagram.com/' target={'blank'}>
+            Instagram
+          </a>
+          <a href='https://github.com/shubhamlahoti01' target={'blank'}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
